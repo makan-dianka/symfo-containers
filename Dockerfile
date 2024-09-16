34be90ce7dd5969b2d32df29c0f63f20bae648fe
Makan DIANKA <makan.dianka@hotmail.com>
2024-09-16 22:12:37 +0200

    intial symfony project. docker-compose

---
+++ Dockerfile
@@ -0,0 +1,58 @@
+#    +--------------------- Dockerfile                     ----------------+    
+#    +                                                                     +    
+#    +            name         Role          Describtion                   +    
+#    +          ----------------------------------------------             +    
+#    +          Steph  ' Bug Hunter       '        -         '             +    
+#    +          Makan  ' Cyber weapon     '        -         '             +    
+#    +                 '                  '                  '             +    
+#    +                 '                  '                  '             +    
+#    +                                                                     +    
+#    +---------------------------------------------------------------------+    
+#                                                
+#    >*____dockerfile_____>_____dockerfile______>_______dockerfile_________>*
+
+
+
+
+
+FROM php:8.3-apache
+
+
+ARG USERNAME
+ARG EMAIL
+ARG APP
+# ENV APP_NAME=${APP}
+
+
+RUN apt update -y \
+        && apt install -y git \
+        && apt install -y curl \ 
+        && apt install -y wget \
+        && apt install -y nano \
+        && apt install -y vim
+
+RUN docker-php-ext-install pdo pdo_mysql mysqli
+
+
+WORKDIR /var/www/
+
+# install composer
+RUN curl -Ss https://getcomposer.org/installer | php
+RUN mv composer.phar /usr/local/bin/composer
+RUN chmod 777 /usr/local/bin/composer
+
+# config git
+RUN git config --global user.name ${USERNAME}
+RUN git config --global user.email ${EMAIL}
+
+# Install symfo
+RUN curl -sS https://get.symfony.com/cli/installer | bash
+RUN mv /root/.symfony5/bin/symfony /usr/local/bin/symfony
+
+
+RUN apt update && apt install npm -y
+
+
+ENTRYPOINT [ "symfony" ]
+
+CMD [ "new", "--webapp", "--version=6.0.*", "monapp" ]
---
+++ start.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/bash
+                            set -e
+# ****************************************************************
+# *                                                              *
+# *                       *10-09-2024*                           *
+# *                                                              *
+# ****************************************************************
+
+info="Please get help with $0 --help"
+help(){
+    echo 
+    echo "# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "# +   10                   /////09///////              24        +"
+    echo "# |   =>    =>    =>       Cyber missile     =>   =>   =>        |"
+    echo "# +                       //////////////                         +"
+    echo "# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo 
+    echo "USAGE: --email your_git_email@domain.com --username your_git_username"
+    echo 
+    echo
+}
+        #get help
+#-------------------------#
+if [[ $1 == "--help" ]];
+then
+    help
+    exit
+fi
+
+
+
+
+
+
+
+######################### Stop script if .env file does not exist ##################################
+if [ ! -f '.env' ]
+then
+echo ".env file does not exist. Please create .env file in current directory and set the variable mentionned in readme."
+exit
+fi
+#############  Please make sure that .env file exist in current directory ##########################
+
+
+
+
+
+
+
+args=$#
+filename=$0
+
+email_flag=$1
+email=$2
+
+username_flag=$3
+username=$4
+
+
+
+build_image(){ #build docker image symfo:1.0
+    # this function takes 6 args to build image symfo:1.0 :
+    # --email exemple@gmail.com --username exemple --appname exemple.
+
+    # if args given to this function not egal to 6 
+    # then print info and stop script
+    if [[ $args -ne 4 ]];
+    then
+        echo "$filename takes 4 arguments"
+        echo $info
+        exit 1
+    fi
+
+
+    # if arg 1 egal to email flag and 
+    # arg 3 egal to flag username and
+    # arg 5 egal to flag appname
+    # then build image with these info
+    # otherwise print info
+    if [[ $email_flag == '--email' && $username_flag == '--username' ]];
+    then
+        docker build --no-cache --build-arg USERNAME=$username \
+                                --build-arg EMAIL=$email \
+                                -t symfo:1.0 .
+    else
+        echo "Flag incorrect. check your flag names"
+        echo $info
+    fi
+}
+
+
+
+
+
+
+create_containers(){ # create containers with docker-compse
+    # this function create containers by launching docker-compose 
+
+    # check if image symfo already exist 
+    # if so then create containers 
+    # otherwise build image symfo and create containers
+    if [[ ! $(docker images | grep symfo) ]]
+    then
+        build_image
+    fi
+
+
+    # check if conatiners already exist 
+    # if so then start containers 
+    # otherwise create containers
+    if [[ ! $(docker-compose ps | grep myapp) ]]
+    then
+        docker-compose --env-file .env up -d
+    else
+        docker-compose start
+    fi
+    
+}
+
+
+
+
+create_containers
